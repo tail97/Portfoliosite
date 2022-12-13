51297b4071a6ea6b3aecb4545cf647f8d0849ea6
한정훈 <tail97@hanmail.net>
2022-12-13 11:10:42 +0900

    첨부파일 구현 및 다운로드 버튼 구현

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+
 
 # Create your models here.
 
@@ -7,12 +9,21 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True) #add 할 때의 시간
     updated_at = models.DateTimeField(auto_now=True) #update 할 때의 시간
-    head_image = models.ImageField(upload_to='blog/images/%Y/%M/%D/',blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank = True)
+    file_upload = models.FileField(upload_to = 'blog/file/%Y/%m/%d/',blank = True )
+    
+    
+    
     def __str__(self):
         return f'[{self.pk}]--{self.title}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+    
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
 
 
